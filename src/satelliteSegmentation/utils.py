2f2e3cb0c689bc02d2f8942cb88acb55547385dc
GonzaloJara <gonza54110@gmail.com>
2026-06-18 19:22:41 -0300

    add class names

--- src/satelliteSegmentation/utils.py
+++ src/satelliteSegmentation/utils.py
@@ -3,68 +3,63 @@ import seaborn as sns
 import numpy as np
 import torch
 
+from satelliteSegmentation.tokenizer import IDX2LABEL
 
-def plot_confusion_matrix(
-    cm: np.ndarray, class_names: list | None = None, normalize: list | None = None
-):
+
+def plot_confusion_matrix(cm: np.ndarray):
     cm = np.array(cm)
+    cm = cm / (cm.sum(axis=1, keepdims=True) + 1e-7)
 
-    if normalize:
-        cm = cm / (cm.sum(axis=1, keepdims=True) + 1e-7)
-
-    plt.figure(figsize=(8, 6))
-
+    plt.figure(figsize=(10, 8))
     sns.heatmap(
         cm,
         annot=True,
         fmt=".2f",
         cmap="Blues",
-        xticklabels=class_names if class_names else range(len(cm)),  # type: ignore
-        yticklabels=class_names if class_names else range(len(cm)),  # type: ignore
+        xticklabels=[IDX2LABEL[i] for i in range(len(IDX2LABEL))],
+        yticklabels=[IDX2LABEL[i] for i in range(len(IDX2LABEL))],
     )
 
     plt.xlabel("Predicción")
     plt.ylabel("Ground Truth")
     plt.title("Matriz de Confusión (normalizada)")
+    plt.xticks(rotation=45, ha="right")
+    plt.yticks(rotation=0)
+    plt.tight_layout()
     plt.show()
 
-def plot_class_metrics(dice: list, iou: list, class_names=None):
 
-    x = np.arange(len(dice))
-
-    plt.figure(figsize=(10, 5))
+def plot_class_metrics(dice: list, iou: list):
+    class_names = [IDX2LABEL[i] for i in range(len(IDX2LABEL))]
+    x = np.arange(len(class_names))
+    plt.figure(figsize=(12, 5))
 
     plt.plot(x, dice, marker="o", label="Dice")
     plt.plot(x, iou, marker="o", label="IoU")
 
-    plt.xticks(
-        x,
-        class_names if class_names else [str(i) for i in x]
-    )
-
+    plt.xticks(x, class_names, rotation=45, ha="right")
     plt.ylim(0, 1)
     plt.grid(True, alpha=0.3)
+
     plt.legend()
     plt.title("Dice e IoU por clase")
-    plt.xlabel("Clase")
-    plt.ylabel("Score")
-
+    plt.tight_layout()
     plt.show()
 
 
 def plot_bar_metrics(dice, iou):
-    x = np.arange(len(dice))
-    plt.figure(figsize=(10, 5))
+    class_names = [IDX2LABEL[i] for i in range(len(IDX2LABEL))]
+    x = np.arange(len(class_names))
+    plt.figure(figsize=(12, 5))
 
     plt.bar(x - 0.2, dice, width=0.4, label="Dice")
     plt.bar(x + 0.2, iou, width=0.4, label="IoU")
 
+    plt.xticks(x, class_names, rotation=45, ha="right")
     plt.ylim(0, 1)
-    plt.xticks(x)
-    plt.xlabel("Clase")
-    plt.ylabel("Score")
-    plt.title("Comparación Dice vs IoU por clase")
-    plt.legend()
     plt.grid(axis="y", alpha=0.3)
 
+    plt.legend()
+    plt.title("Dice vs IoU por clase")
+    plt.tight_layout()
     plt.show()
